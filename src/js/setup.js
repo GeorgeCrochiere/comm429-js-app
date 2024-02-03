340469f73d6afa1bfeeb82481eef559fc56ce818
George D. Crochiere <46457797+GeorgeCrochiere@users.noreply.github.com>
2024-02-03 14:26:28 -0500

    Created setup.js to allow for dynamic values within the selector based on the categories present in the quotes.
    Given how each quote has a category, with the goal of being able to select a quote by each category, added the ability to dynamically obtain a list of unique categories, while also having the option for a random item.

---
+++ src/js/setup.js
@@ -0,0 +1,29 @@
+setSelector();
+
+let dataCategories = [];
+data.forEach((entry) => {
+    dataCategories.push(entry.category);
+});
+dataCategories = dataCategories.filter(unique);
+
+// Filter function for unique values
+function unique(value, index, array) {
+    return array.indexOf(value) === index
+}
+
+function setSelector() {
+    const selector = document.querySelector("select");
+
+    const defaultOption = document.createElement("option");
+    defaultOption.value = -1;
+    defaultOption.text = "Random";
+    selector.appendChild(defaultOption);
+
+    dataCategories.forEach((category) => {
+        const option = document.createElement("option");
+        option.value = category;
+        option.text = category;
+        selector.appendChild(option);
+    });
+};
+
