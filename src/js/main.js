1393d55a6dd320d4586e5bce5ecf5c5cf43cbdf5
George D. Crochiere <46457797+GeorgeCrochiere@users.noreply.github.com>
2024-02-03 14:27:32 -0500

    Started progress on quote generation
    Added the necessary event listeners and elements to determine which quote will be generated. Working on factory that creates the quote object given the data.

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,19 @@
-testJS();
+document.querySelector("button").addEventListener(() => {
+    // Get category
+    const category = document.querySelector("select").value;
+    let newQuote;
+    if (category === -1) {
+        const rand = Math.floor(Math.random() * data.length);
+        newQuote = quoteFactory(data[rand]);
+    } else {
+        const filteredData = data.filter((quote) => quote.category == category);
+        const rand = Math.floor(Math.random() * filteredData.length);
+        newQuote = quoteFactory(filteredData[rand]);
+    }
 
-function testJS() {
-    const paragraphElement = document.createElement('p');
-    paragraphElement.textContent = String('If this element is present, Hello World from JavaScript!');
-    paragraphElement.style.color = 'blue';
+    document.querySelector("section").appendChild(newQuote);
+});
 
-    const jsSection = document.getElementById('jsContentTest');
-    jsSection.appendChild(paragraphElement);
+function quoteFactory(quoteObject) {
+
 }
